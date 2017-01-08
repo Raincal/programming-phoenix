b27b740f81a2b1a2f1f4a899b1779ab54b7426e6
Raincal <cyj94228@gmail.com>
2017-01-08 19:41:46 +0800

    Chapter 4 Ecto and Changesets

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -1,3 +1,23 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :username, :password, :password_hash])
+    |> validate_required([:name, :username, :password])
+    |> validate_length(:username, min: 3, max: 20)
+    |> unique_constraint(:username)
+  end
 end
