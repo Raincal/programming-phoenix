a1ddf651925ec1c8aacf6f85b39780a1b49e6de3
Raincal <cyj94228@gmail.com>
2017-01-09 00:44:07 +0800

    Chapter 5 Authenticating Users

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -15,9 +15,27 @@ defmodule Rumbl.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :username, :password, :password_hash])
-    |> validate_required([:name, :username, :password])
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
     |> validate_length(:username, min: 3, max: 20)
     |> unique_constraint(:username)
+  end
+
+  def registration_changeset(struct, params) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
